695d119971d712977e99117c8a63a9c522ce91ef
wuqiao <wuqiao309@163.com>
2023-11-04 16:29:33 +0800

    update conifg

--- copy2here.sh
+++ copy2here.sh
@@ -4,12 +4,12 @@
 DISK_IDENTIFIER=`diskutil list | grep 'EFI' | grep -E 'disk0|disk1' | awk -F ' ' '{print $6}'`
 
 # 挂载EFI
-diskutil mount $DISK_IDENTIFIER
+sudo diskutil mount $DISK_IDENTIFIER
 sleep 1
 
 # 更新EFI
-rm -rf /Users/wuqiao/IdeaProjects/MsiZ690-A_13600KF_6950XT_IntelA210_EFI/EFI
-cp -R '/Volumes/EFI/EFI' /Users/wuqiao/IdeaProjects/MsiZ690-A_13600KF_6950XT_IntelA210_EFI/EFI
+rm -rf /Users/wuqiao/IdeaProjects/MsiZ690-MsiZ690-A_13600KF_6950XT_IntelA210_EFI_Hackintosh/EFI
+cp -R '/Volumes/EFI/EFI' /Users/wuqiao/IdeaProjects/MsiZ690-MsiZ690-A_13600KF_6950XT_IntelA210_EFI_Hackintosh/EFI
 
 # 卸载EFI
 # diskutil unmount $DISK_IDENTIFIER
