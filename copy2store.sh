21b11580f8bb206762828769a5e7c13a46f75738
wuqiao <wuqiao309@163.com>
2023-11-04 16:28:52 +0800

    update conifg

--- copy2store.sh
+++ copy2store.sh
@@ -4,17 +4,17 @@
 DISK_IDENTIFIER=`diskutil list | grep 'EFI' | grep -E 'disk0|disk1' | awk -F ' ' '{print $6}'`
 
 # 挂载EFI
-diskutil mount $DISK_IDENTIFIER
+sudo diskutil mount $DISK_IDENTIFIER
 sleep 1
 
 # 备份文件到硬盘
-rm -rf '/Volumes/机械硬盘1/EFI-backup-usb'
-cp -R /Users/wuqiao/IdeaProjects/MsiZ690-A_13600KF_6950XT_IntelA210_EFI/EFI '/Volumes/机械硬盘1/EFI-backup-usb'
+rm -rf '/Users/wuqiao/Desktop/EFI-backup-usb'
+cp -R /Volumes/EFI/EFI '/Users/wuqiao/Desktop/EFI-backup-usb'
 
 # 更新EFI
 cd '/Volumes/EFI'
 rm -rf EFI
-cp -R /Users/wuqiao/IdeaProjects/MsiZ690-A_13600KF_6950XT_IntelA210_EFI/EFI '/Volumes/EFI/EFI'
+cp -R /Users/wuqiao/IdeaProjects/MsiZ690-A_13600KF_6950XT_IntelA210_EFI_Hackintosh/EFI '/Volumes/EFI/EFI'
 
 # 卸载EFI
 # diskutil unmount $DISK_IDENTIFIER
