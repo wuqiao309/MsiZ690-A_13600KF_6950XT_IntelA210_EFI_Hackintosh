bbb91e22addbc464eb39ca937e0210e8532de61c
wuqiao <wuqiao309@163.com>
2024-03-16 16:21:43 +0800

    shell

--- copy2here.sh
+++ copy2here.sh
@@ -4,7 +4,7 @@
 DISK_IDENTIFIER=`diskutil list | grep 'EFI' | grep -E 'disk0|disk1' | awk -F ' ' '{print $6}'`
 
 # 挂载EFI
-sudo diskutil mount $DISK_IDENTIFIER
+echo "1111" | sudo diskutil mount $DISK_IDENTIFIER
 sleep 1
 
 # 更新EFI
--- copy2store.sh
+++ copy2store.sh
@@ -4,7 +4,7 @@
 DISK_IDENTIFIER=`diskutil list | grep 'EFI' | grep -E 'disk0|disk1' | awk -F ' ' '{print $6}'`
 
 # 挂载EFI
-sudo diskutil mount $DISK_IDENTIFIER
+echo "1111" | sudo diskutil mount $DISK_IDENTIFIER
 sleep 1
 
 # 备份文件到硬盘
@@ -13,7 +13,7 @@ cp -R /Volumes/EFI/EFI '/Users/wuqiao/Desktop/EFI-backup-usb'
 
 # 更新EFI
 cd '/Volumes/EFI'
-rm -rf EFI
+rm -rf ./*
 cp -R /Users/wuqiao/IdeaProjects/MsiZ690-A_13600KF_6950XT_IntelA210_EFI_Hackintosh/EFI '/Volumes/EFI/EFI'
 
 # 卸载EFI
